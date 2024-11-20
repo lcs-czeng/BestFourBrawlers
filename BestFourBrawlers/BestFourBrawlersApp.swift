1c217ad9884228f7e4a3cfd3cf2d136130469c4e
Courage Zeng <czeng@lcs.on.ca>
2024-11-20 10:55:00 -0500

    Initial Commit

---
+++ BestFourBrawlers/BestFourBrawlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFourBrawlersApp.swift
+//  BestFourBrawlers
+//
+//  Created by 曾梓恒 on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BestFourBrawlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
